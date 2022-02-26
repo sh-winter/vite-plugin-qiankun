9856c20d441a9d4509e90633f5570f2a6a97995a
sh-winter <2943331588@qq.com>
2022-02-26 10:10:32 +0800

    更新示例中的 vite 版本

--- rollup.config.js
+++ rollup.config.js
@@ -5,10 +5,11 @@ import pkg from './package.json'
 const banner = `
 /*!
   * ${pkg.name} v${pkg.version}
-  * (c) 2021-${new Date().getFullYear()} sh-winter
+  * (c) 2022-${new Date().getFullYear()} sh-winter
   * Released under the MIT License.
   */
 `
+<<<<<<< Updated upstream
 
 /**
  * 生成 rollup config
@@ -33,6 +34,22 @@ function genConfig(format) {
       name: pkg.name,
       exports: 'named'
     }
+=======
+export default defineConfig({
+  input: ['./src/index.ts', './src/helper.ts'],
+  external: [
+    ...Object.keys(pkg.dependencies)
+  ],
+  plugins: [
+    typescript()
+  ],
+  output: {
+    format: 'esm',
+    banner,
+    dir: 'dist',
+    name: pkg.name,
+    exports: 'auto'
+>>>>>>> Stashed changes
   }
 }
 
